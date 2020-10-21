c4e527b86046820cd8e40173401ad9187d4c81f4
unknown <alejandrojzeballos@gmail.com>
2020-10-21 01:25:56 -0400

    Añadiendo bootstrap

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,13 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
-        $this->load->modal('Persona_model');
+        $this->load->model('Persona_model');
 
 	} 
 
 	public function index()
 	{
-       $this->Persona_model->insertarPersona();
 		echo 'HOLA MUNDO';
 	}
 }
