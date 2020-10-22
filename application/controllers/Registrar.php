1a9b29dcdbb589b5151c6a0bf6fa80691665e75f
unknown <alejandrojzeballos@gmail.com>
2020-10-21 20:01:23 -0400

    añadiendo la vista registrar usuario

--- application/controllers/Registrar.php
+++ application/controllers/Registrar.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Registrar extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
         $this->load->model('Persona_model');
@@ -12,13 +12,7 @@ class Home extends CI_Controller {
 	{
 		$this->load->view('templates/header');
 		$this->load->view('templates/nav');
-		$this->load->view('home');
-		$this->load->view('templates/footer');
-	}
-	public function prueba(){
-		$this->load->view('templates/header');
-		$this->load->view('templates/nav');
-		$this->load->view('home');
+		$this->load->view('registrar');
 		$this->load->view('templates/footer');
 	}
 }
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,4 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="<?php echo base_url();?>assets/js/bootstrap.min.js" ></script>
   </body>
+    <footer class="mt-5 p-3 text-center bg-primary align-items-end">
+               Asistente AEE &copy;
+    </footer>
 </html>
