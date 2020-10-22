cb6fdab717a325a348ebffdc536a64cc37450490
unknown <alejandrojzeballos@gmail.com>
2020-10-21 23:08:02 -0400

    añadiendo el persona_model

--- application/controllers/Registrar.php
+++ application/controllers/Registrar.php
@@ -17,14 +17,54 @@ class Registrar extends CI_Controller {
     }
     
     public function registrarUsuario(){
-        $registro=$this->input->post('u_registrar');
-        $u_nombre=$this->input->post('u_nombre');
-        $u_apellido = $this->input->post('u_apellido');
-        $u_date = $this->input->post('u_date');
-        $u_telefono = $this->input->post('u_telefono');
-        $u_genero = $this->input->post('u_genero');
-        $p_email = $this->input->post('p_email');
-        $p_password = $this->input->post('p_password');
-        echo $registro.' '.$u_nombre.' '.$u_apellido.' '.$u_date.' '.$u_telefono.' '.$u_genero.' '.$p_email.' '.$p_password;
+        
+        if($this->input->post('u_registrar')){
+            $u_nombre=$this->input->post('u_nombre');
+            $u_apellido = $this->input->post('u_apellido');
+            $u_date = $this->input->post('u_date');
+            $u_telefono = $this->input->post('u_telefono');
+            $u_genero = $this->input->post('u_genero');
+            $p_email = $this->input->post('p_email');
+            $p_password = $this->input->post('p_password');
+            echo ' '.$u_nombre.' '.$u_apellido.' '.$u_date.' '.$u_telefono.' '.$u_genero.' '.$p_email.' '.$p_password;
+        }
+        $persona_detalles = array(
+            'p_email' => $p_email,
+            'p_password' => $p_password
+        );
+        var_dump($persona_detalles);
+        if(!$this->Persona_model->crearPersona($persona_detalles)){
+            $this->output->set_status_header(500);
+            echo json_encode(array('msg' => 'Error al crear la instancia Persona'));
+            exit;
+        }else{
+            $FK_id_p=$this->Persona_model->obtenerId($p_email);
+            if($FK_id_p==null){
+                $this->output->set_status_header(500);
+                echo json_encode(array('msg' => 'Error al obtener el id'));
+                exit;
+            }
+            $usuario_detalles = array(
+                'u_nombre' => $u_nombre,
+                'u_apellido' => $u_apellido,
+                'u_fechanac' => $u_date,
+                'u_telefono' => $u_telefono,
+                'u_sexo' => $u_genero,
+                'FK_p_id' =>$FK_id_p
+            );
+            var_dump($usuario_detalles);
+            if(!$this->Persona_model->crearUsuario($usuario_detalles)){
+                $this->output->set_status_header(500);
+                echo json_encode(array('msg' => 'Error al crear usuario'));
+            }
+            $idPersona=$this->Persona_model->obtenerId($p_email);
+            if($idPersona==null){
+                //header error
+                exit;
+            }
+
+            //redireccionar
+        }
+        
 	}
 }
--- application/models/Persona_model.php
+++ application/models/Persona_model.php
@@ -2,8 +2,26 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Persona_model extends CI_Model{
-    public function insertarPersona($user_data){
-              
+    public function insertarPersona($persona_detalles){
+        if(!$this->db->insert("persona",$persona_detalles))
+             return false;
+        else
+            return true; 
+    }
+    public function crearPersona($persona_detalles){
+        if(!$this->db->insert("persona",$persona_detalles))
+             return false;
+        else
+            return true; 
+    }
+    public function obtenerId($email){
+        $query = $this->db->select('PK_p_id')
+                ->where('p_email', $email)
+                ->get('persona');
+        if(isset($query))
+            return $query->row_array();
+        else
+            return null;
     }
 }
 
