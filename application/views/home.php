f296d720edf6414859738207e6f58f80ec3a4ab9
unknown <alejandrojzeballos@gmail.com>
2020-10-22 21:26:06 -0400

    añadiendo el footer con materialize

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,19 @@
 <div class="container">
     <div class="row">
         <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
+        <div class="col-12"><h1>Pagina Home</h1></div>
     </div>
 </div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -7,12 +7,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!--JavaScript at end of body for optimized loading-->
     <script type="text/javascript" src="<?php echo base_url();?>assets/js/materialize.min.js"></script>
   </body>
-    <footer class="bg-primary fixed-bottom">
+    <!-- <footer class="bg-primary fixed-bottom">
         <div class="container mt-5 p-3 text-center">
             <div class="row">
                 <div class="col-12">Asistente AEE &copy;</div>
             </div>
         </div>
                
-    </footer>
+    </footer> -->
+
+    <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
 </html>
