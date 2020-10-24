8cf37f0d6785acb28a8102113d7b35138ad50305
unknown <alejandrojzeballos@gmail.com>
2020-10-23 22:30:17 -0400

    añadiendo el logout en el nav de usuario

--- application/views/usuario/templatesUsu/nav.php
+++ application/views/usuario/templatesUsu/nav.php
@@ -10,9 +10,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="nav-wrapper">
         <a href="#" class="brand-logo">Logo</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Ingresar</a></li>
+            <li><a href="#">Ingresar</a></li>
             <li><a href="badges.html">Registrate</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><a href="<?php echo site_url();?>registrar/logoutUsuario">LogOut</a></li>
         </ul>
         </div>
     </nav>
