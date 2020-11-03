84dca54d4bbb3254d8d9b2287603bad2bc3dd7b5
unknown <alejandrojzeballos@gmail.com>
2020-11-03 02:02:11 -0400

    probar en el servidor el envio de emails

--- application/controllers/DoctorUsuario.php
+++ application/controllers/DoctorUsuario.php
@@ -91,16 +91,73 @@ class DoctorUsuario extends CI_Controller {
         $dataPersona=$this->Persona_model->obtenerPersona($dataDoctor['FK_d_p_id']);
         $data=array_merge($dataPersona,$dataDoctor);
         var_dump($data);
-        $this->enviaEmail('alejandrojzeballos','vacio','hi','mi primer mensaje  con php');
+        if($this->enviaEmail($data['p_email'],$u_d_asunto,$u_d_mensaje)){
+            echo "<script>alert('correo Enviado')</script>";
+            redirect('registrar/ultimaRespuestaUsu', 'refresh');
+            exit;
+        }else{
+                //no se pudo
+        }
 
 
     }
-    public function enviaEmail($to,$from,$subject,$mensaje){
+    public function enviaEmail($to,$subject,$mensaje){
+        $this->email->set_newline("\r\n");
+        //Indicamos el protocolo a utilizar
+        $config['protocol'] = 'smtp';
+         
+       //El servidor de correo que utilizaremos
+        $config["smtp_host"] = 'smtp.gmail.com';
+         
+       //Nuestro usuario
+        $config["smtp_user"] = 'asistenteaee@gmail.com';
+         
+       //Nuestra contraseña
+        $config["smtp_pass"] = 'asistente433';   
+         
+       //El puerto que utilizará el servidor smtp
+        $config["smtp_port"] = '25';
+        
+       //El juego de caracteres a utilizar
+        $config['charset'] = 'utf-8';
+ 
+       //Permitimos que se puedan cortar palabras
+        $config['wordwrap'] = TRUE;
+         
+       //El email debe ser valido 
+       $config['validate'] = true;
+       
+        
+      //Establecemos esta configuración
+        $this->email->initialize($config);
+ 
+      //Ponemos la dirección de correo que enviará el email y un nombre
+        $this->email->from('asistenteaee@gmail.com', 'Alejandro Zeballos');
+         
+      /*
+       * Ponemos el o los destinatarios para los que va el email
+       * en este caso al ser un formulario de contacto te lo enviarás a ti
+       * mismo
+       */
         $this->email->to($to);
-        $this->email->from('ale03zeballos@gmail.com');
+         
+      //Definimos el asunto del mensaje
         $this->email->subject($subject);
+         
+      //Definimos el mensaje a enviar
         $this->email->message($mensaje);
-        $this->email->send();
+         
+        //Enviamos el email y si se produce bien o mal que avise con una flasdata
+        if($this->email->send()){
+            echo 'Email enviado correctamente';
+            echo $this->email->print_debugger(array('headers'));
+            return false;
+        }else{
+            //pruebas en el host, no puedo definirlo en localhost
+            echo 'No se a enviado el email';
+            echo $this->email->print_debugger(array('headers'));
+            return true;
+        }
     }
     public function logoutDoctor(){
 		session_unset();
